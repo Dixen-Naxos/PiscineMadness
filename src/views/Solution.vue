d1a0cbb1d7d00146ffc1f0e8194c2eabe2ccd6c4
Armand <armanddailly@gmail.com>
2023-09-21 13:30:11 +0200

    Update Solution.vue

--- src/views/Solution.vue
+++ src/views/Solution.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="solution">
-    <h1>Solution : </h1>
+    <h1>Bravo, voici les indices : </h1>
+    <p>Ce n'est pas un crabe</p>
+    <p>Ca possède de magnifiques yeux de la couleur du logo du C</p>
   </div>
 </template>
 
